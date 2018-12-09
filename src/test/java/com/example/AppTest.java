66e9903b91484d9b39e21e4d041a0b6aace71567
Łukasz Wierciński <lukasz.wiercinski@gmail.com>
2018-12-09 23:03:46 +0100

    Fix

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,29 +2,18 @@ package com.example;
 
 import cloud.localstack.DockerTestUtils;
 import cloud.localstack.LocalstackExtension;
+import cloud.localstack.LocalstackTestRunner;
 import cloud.localstack.docker.LocalstackDockerExtension;
 import cloud.localstack.docker.annotation.LocalstackDockerProperties;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(LocalstackDockerExtension.class)
+@RunWith(LocalstackTestRunner.class)
 @LocalstackDockerProperties(randomizePorts = true, services = { "dynamodb" })
 class AppTest {
-
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void doIt() {
